88d3739f7498eefb2b6df014845995d68c66664b
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-12-11 22:58:03 +0100

    Some camera position GUI.

--- ui/Scene.elm
+++ ui/Scene.elm
@@ -2,10 +2,11 @@ module Scene exposing (Model, Msg, init, update, view, subscriptions)
 
 import AnimationFrame exposing (diffs)
 import Chambre exposing (Chambre)
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, p, fieldset, input, label, text, table, tr, td)
 import Html.Attributes as Attr
-import Math.Vector3 exposing (vec3)
-import Math.Matrix4 exposing (Mat4, makePerspective)
+import Html.Events as Evts
+import Math.Vector3 exposing (Vec3, vec3)
+import Math.Matrix4 exposing (Mat4, mul, makePerspective, makeLookAt)
 import Pyramid exposing (Pyramid)
 import Task as Task
 import Time exposing (Time, inSeconds)
@@ -16,22 +17,32 @@ type alias Model =
     { perspective : Mat4
     , chambre : Chambre
     , pyramid : Pyramid
+    , cameraPos : CameraPos
     , errMsg : String
     }
 
 
 type Msg
     = Animate Time
+    | SwitchTo CameraPos
     | TextureLoaded (List WebGL.Texture)
     | TextureFailed WebGL.Error
+
+
+type CameraPos
+    = DownInCorner
+    | UpInCorner
+    | DownAtSide
+    | UpAtSide
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { perspective =
             makePerspective 45 (toFloat sceneWidth / toFloat sceneHeight) 0.1 100
-      , chambre = Chambre.make (vec3 0 -5 -25) (vec3 1 1 1)
-      , pyramid = Pyramid.make (vec3 0 -3 -25) (vec3 1 1 1)
+      , chambre = Chambre.make (vec3 0 0 -50) (vec3 5 5 5)
+      , pyramid = Pyramid.make (vec3 0 6 -50) (vec3 5 5 5)
+      , cameraPos = DownInCorner
       , errMsg = ""
       }
     , tryLoadTextures
@@ -52,6 +63,9 @@ update msg model =
             , Cmd.none
             )
 
+        SwitchTo pos ->
+            ( { model | cameraPos = pos }, Cmd.none )
+
         TextureLoaded [ floorTile, pyramidTile ] ->
             ( { model
                 | chambre = Chambre.setFloorTile floorTile model.chambre
@@ -71,15 +85,42 @@ view : Model -> Html Msg
 view model =
     div []
         [ viewScene model
+        , viewCameraControl model
         , p [] [ text model.errMsg ]
         ]
 
 
 viewScene : Model -> Html Msg
 viewScene model =
-    WebGL.toHtml [ Attr.width sceneWidth, Attr.height sceneHeight ] <|
-        Chambre.view model.perspective model.chambre
-            ++ Pyramid.view model.perspective model.pyramid
+    let
+        vp =
+            makeViewerPerspective model
+    in
+        WebGL.toHtml [ Attr.width sceneWidth, Attr.height sceneHeight ] <|
+            Chambre.view vp model.chambre
+                ++ Pyramid.view vp model.pyramid
+
+
+viewCameraControl : Model -> Html Msg
+viewCameraControl model =
+    div [ Attr.style [ ( "width", "800px" ), ( "border", "solid" ) ] ]
+        [ p [] [ text "Camera Positions" ]
+        , table []
+            [ tr [] [ td [] [ radio "Down in corner" <| SwitchTo DownInCorner ] ]
+            , tr [] [ td [] [ radio "Up in corner" <| SwitchTo UpInCorner ] ]
+            , tr [] [ td [] [ radio "Down at side" <| SwitchTo DownAtSide ] ]
+            , tr [] [ td [] [ radio "Up at side" <| SwitchTo UpAtSide ] ]
+            ]
+        ]
+
+
+radio : String -> Msg -> Html Msg
+radio value msg =
+    label [ Attr.style [ ( "padding", "20px" ) ] ]
+        [ input [ Attr.type_ "radio", Attr.name "camera", Evts.onClick msg ]
+            []
+        , text value
+        ]
 
 
 subscriptions : Model -> Sub Msg
@@ -102,11 +143,36 @@ tryLoadTextures urls =
         Task.sequence (List.map WebGL.loadTexture urls)
 
 
+getCameraPos : Model -> ( Vec3, Vec3 )
+getCameraPos model =
+    case model.cameraPos of
+        DownInCorner ->
+            ( vec3 -34 2 -79, vec3 0 10 -50 )
+
+        UpInCorner ->
+            ( vec3 -34 20 -79, vec3 0 3 -50 )
+
+        DownAtSide ->
+            ( vec3 0 2 -84, vec3 0 10 -50 )
+
+        UpAtSide ->
+            ( vec3 0 20 -84, vec3 0 3 -50 )
+
+
+makeViewerPerspective : Model -> Mat4
+makeViewerPerspective model =
+    let
+        ( eye, lookAt ) =
+            getCameraPos model
+    in
+        mul model.perspective <| makeLookAt eye lookAt (vec3 0 1 0)
+
+
 sceneWidth : Int
 sceneWidth =
-    500
+    800
 
 
 sceneHeight : Int
 sceneHeight =
-    400
+    600
